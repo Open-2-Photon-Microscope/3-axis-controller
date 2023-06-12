82a1f40c3496824808591e732a4d49fe39f01766
Andre Maia Chagas <andremaia.chagas@gmail.com>
2023-06-12 16:36:56 +0100

    Update main.py

--- micropython/main.py
+++ micropython/main.py
@@ -1,3 +1,17 @@
-import controller
+import Stepper
 
-control = control.controller()
+from machine import Pin
+'''
+IN1 -->  16
+IN2 -->  17
+IN3 -->  5
+IN4 -->  18
+'''
+s1 = Stepper.create(mode= "HALF_STEP",
+                    pin1=Pin(16,Pin.OUT),
+                    pin2=Pin(17,Pin.OUT),
+                    pin3=Pin(5,Pin.OUT),
+                    pin4=Pin(18,Pin.OUT),
+                    delay=5)
+
+s1.step(100)
